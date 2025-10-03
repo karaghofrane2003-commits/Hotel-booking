ceb553461252a9d0efc3282619b9b0929a7d53ea
ran <ran@gmail.com>
2025-10-03 11:47:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projet Collaboratif - React + Vite + Tailwind
+
+## Installation
+
+```bash
+npm install
+```
+
+## Démarrage du serveur
+
+```bash
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
